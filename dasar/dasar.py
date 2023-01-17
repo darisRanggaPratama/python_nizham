aa6d9b44ba5bc5b25a3cab14eb610df4359d2333
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-01-17 19:58:56 +0700

    initial commit

---
+++ dasar/dasar.py
@@ -0,0 +1,17 @@
+import turtle
+
+t = turtle.Turtle()
+# (lebar, tinggi, x, y)
+turtle.setup(500, 500, 1030, 300)
+t.screen.bgcolor('black')
+
+t.pencolor('red')
+t.speed(1)
+# fd: forward = maju
+t.fd(100)
+t.lt(90)
+t.fd(200)
+t.rt(90)
+t.fd(100)
+
+turtle.done()
