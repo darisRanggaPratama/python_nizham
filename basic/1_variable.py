f36077326188c0f74c50af5515614aef316936e9
darisRanggaPratama <raden.rangga.pratama@gmail.com>
2023-08-02 15:44:13 -0700

    initial commit

---
+++ basic/1_variable.py
@@ -0,0 +1,28 @@
+a = 1988
+print(a)
+
+tipe = type(a)
+print(tipe)
+
+idx = id(a)
+print(idx)
+
+nama = 'Nizham'
+print(nama)
+
+besar = nama.upper()
+print(besar)
+
+kecil = nama.lower()
+print(kecil)
+
+for x in nama:
+    print(x)
+    
+i = 1
+while i <= len(nama):
+    print(i)
+    i += 1
+    
+
+
