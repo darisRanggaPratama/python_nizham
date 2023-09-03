c82f87e16c6d322b58cb93f86a6e29ee6f57ac13
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-09-04 06:30:25 +0700

    initial commit

--- py_xl/RUNS/ws_decimal - Copy.py
+++ py_xl/RUNS/ws_decimal - Copy.py
@@ -2,22 +2,22 @@ from openpyxl import *
 from openpyxl.utils import *
 
 # Isi Nama File
-#path = input('\nDecimal Value in Worksheet\nFile Name: ')
+path = input('\nDecimal Value in Worksheet\nFile Name: ')
 
 # Akses file Excel
-wbook = load_workbook('sample.xlsx')
+wbook = load_workbook(path)
 
 # Tampilkan Worksheet tersedia
-# print(f'Available Worksheet:\n{wbook.sheetnames}')
+print(f'Available Worksheet:\n{wbook.sheetnames}')
 
 # Pilih worksheet yang akan Anda periksa
-# wsheet = input('Get 1 Worksheet: ')
-sheet = wbook['GAJI']
+wsheet = input('Get 1 Worksheet: ')
+sheet = wbook[wsheet]
 
 # Maksimal kolom terisi nilai
 max = get_column_letter(sheet.max_column)
 
-print(f'\nSearch in: ;\nDECIMAL VALUE ;\n\nNo ; Cell ; Value ;')
+print(f'\nSearch in: {wsheet};\nDECIMAL VALUE ;\n\nNo ; Cell ; Value ;')
 
 # Huruf-Kolom ke Angka-Kolom
 start = 0
